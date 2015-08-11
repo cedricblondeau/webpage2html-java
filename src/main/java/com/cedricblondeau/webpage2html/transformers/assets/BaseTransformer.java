bce91db5bc06b21a8af2ef8962050f6d64c026dc
Cedric Blondeau <cedric.blondeau.fr@gmail.com>
2015-08-11 20:02:15 +0200

    BaseTransformer: convert data to base64

---
+++ src/main/java/com/cedricblondeau/webpage2html/transformers/assets/BaseTransformer.java
@@ -0,0 +1,32 @@
+package com.cedricblondeau.webpage2html.transformers.assets;
+
+import java.util.Base64;
+
+public class BaseTransformer {
+
+    protected byte[] data;
+    protected Base64.Encoder encoder;
+    protected String mediaType;
+
+    /**
+     * @param mediaType (e.g: text/css, text/javascript, etc.)
+     */
+    public BaseTransformer(String mediaType) {
+        this.encoder = Base64.getEncoder();
+        this.mediaType = mediaType;
+    }
+
+    /**
+     * @param data
+     */
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+    /**
+     * @return String A String containing the resulting Base64 encoded characters
+     */
+    public String getBase64() {
+        return String.format("data:%s;base64,%s", mediaType, encoder.encodeToString(data));
+    }
+}
