be736ac1fc64e30aac22a45ac9054f731d4ca4e8
Cedric Blondeau <cedric.blondeau.fr@gmail.com>
2015-08-12 16:58:31 +0200

    Cache mocked resource for unit testing

--- src/test/java/com/cedricblondeau/webpage2html/transformers/assets/CssTransformerTest.java
+++ src/test/java/com/cedricblondeau/webpage2html/transformers/assets/CssTransformerTest.java
@@ -3,6 +3,7 @@ package com.cedricblondeau.webpage2html.transformers.assets;
 import com.cedricblondeau.webpage2html.http.HttpCache;
 import com.cedricblondeau.webpage2html.http.resource.HttpDummyResource;
 import com.cedricblondeau.webpage2html.http.resource.IHttpResource;
+import com.cedricblondeau.webpage2html.transformers.http.HttpCacheUtils;
 import junit.framework.TestCase;
 
 import java.net.MalformedURLException;
@@ -32,25 +33,13 @@ public class CssTransformerTest extends TestCase {
     }
 
     public void testWithPngImage() {
-        try {
-            // Cache some data
-            String imageBase64 = "WW91IGRpZG4ndCBzYXkgdGhlIG1hZ2ljIHdvcmQh";
-            IHttpResource pngResource = new HttpDummyResource(
-                    new URL("http://www.cedricblondeau.com/img/test.png"),
-                    "image/png",
-                    null,
-                    Base64.getDecoder().decode(imageBase64)
-            );
-            HttpCache.getInstance().put(pngResource.getUrl().toExternalForm(), pngResource);
-
-            // Transform background property
-            String css = "background: url('/img/test.png');";
-            CssTransformer cssTransformer = new CssTransformer(css, cssUrl);
-            String expectedCss = String.format("background: url(\'data:%s;base64,%s\');", pngResource.getMediaType(), imageBase64);
-            assertEquals("Image URL must be transformed to Base64", expectedCss, cssTransformer.getContent());
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
+        String base64 = "WW91IGRpZG4ndCBzYXkgdGhlIG1hZ2ljIHdvcmQh";
+        String mediaType = "image/png";
+        HttpCacheUtils.cacheMockResource("http://www.cedricblondeau.com/img/test.png", mediaType, base64);
+        String css = "background: url('/img/test.png');";
+        CssTransformer cssTransformer = new CssTransformer(css, cssUrl);
+        String expectedCss = String.format("background: url(\'data:%s;base64,%s\');", mediaType, base64);
+        assertEquals("Image URL must be transformed to Base64", expectedCss, cssTransformer.getContent());
     }
 
 }
---
+++ src/test/java/com/cedricblondeau/webpage2html/transformers/http/HttpCacheUtils.java
@@ -0,0 +1,27 @@
+package com.cedricblondeau.webpage2html.transformers.http;
+
+import com.cedricblondeau.webpage2html.http.HttpCache;
+import com.cedricblondeau.webpage2html.http.resource.HttpDummyResource;
+import com.cedricblondeau.webpage2html.http.resource.IHttpResource;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Base64;
+
+public class HttpCacheUtils {
+
+    public static void cacheMockResource(String url, String mediaType, String base64) {
+        try {
+            IHttpResource resource = new HttpDummyResource(
+                    new URL(url),
+                    mediaType,
+                    null,
+                    Base64.getDecoder().decode(base64)
+            );
+            HttpCache.getInstance().put(resource.getUrl().toExternalForm(), resource);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
